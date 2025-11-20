acee5e1e3e8325deee62530c01bfe925defb787b
Nick Esselman <nick.esselman@gmail.com>
2025-11-20 14:25:45 +0100

    Simplify styling for groenteboer pages

--- src/index.php
+++ src/index.php
@@ -15,9 +15,6 @@ $pageFile = $routes[$page] ?? $routes['home'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Groene Haven Markt</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/styles.css">
 </head>
 <body>
