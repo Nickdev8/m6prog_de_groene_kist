48a850bbcfe6d2528310b1e9163692120a4ed7db
Nick Esselman <nick.esselman@gmail.com>
2025-11-25 15:08:26 +0100

    Set simple controller routing in digipost index

--- m6prog_digipost/src/index.php
+++ m6prog_digipost/src/index.php
@@ -1,20 +1,15 @@
 <?php
-require_once __DIR__ . '/db.php';
+include_once __DIR__ . '/db.php';
 
-$db = database_connect();
+$request_url = explode('/', $_SERVER['REQUEST_URI']);
 
-$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '', '/');
-$segments = $path == '' ? [] : explode('/', $path);
-$resource = $segments[0] ?? '';
-
-$routes = [
-    'berichten' => __DIR__ . '/controllers/bericht.php',
-];
-
-if (!isset($routes[$resource])) {
-    http_response_code(404);
-    echo 'NOT FOUND';
-    exit;
+$controllers = ['bericht'];
+foreach ($controllers as $page) {
+    if ($request_url[1] == $page) {
+        include_once __DIR__ . '/controllers/' . $page . '.php';
+        exit;
+    }
 }
 
-require $routes[$resource];
+http_response_code(404);
+exit;
